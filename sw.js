305320f855ca171ef00b0ef51815c35f63e15759
snap <sebastian.latting@gmail.com>
2026-09-12 18:16:24 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'velocity-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest',
+                './icon-180.png', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE)
+    .then(c => Promise.allSettled(ASSETS.map(a => c.add(a))))
+    .then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(cached =>
+      cached || fetch(e.request).then(res => {
+        if (res.ok && new URL(e.request.url).origin === self.location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
